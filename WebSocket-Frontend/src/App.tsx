4d5e98748fc52c3d98a79288fd8c2066c415eddc
Rahul Yadav <ry9064526713@gmail.com>
2026-04-02 23:33:56 +0530

    added the frontend folders and removed some errors

---
+++ WebSocket-Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <input type="text" placeholder="enter your message"></input>
+      <button>Send</button>
+    </div>
+  )
+}
+
+export default App
